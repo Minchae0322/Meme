de1e584eed9a096098f37940745137c06a959282
Minchae0322 <jmcabc0322@gmail.com>
2023-08-02 21:00:28 +0900

    axios 를 사용한 post 요청 보내기

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,14 +1,39 @@
 
 <script setup lang = "ts">
 import {ref} from "vue";
+import axios from 'axios'
 
-const count = ref(0)
+const title = ref("")
+const content = ref("")
 
+
+const write = function () {
+  axios.post("http://localhost:8080/posts", {
+    title: title.value,
+    content: content.value
+  })
+};
 </script>
 
-<template>
-  <p>안녕하세요</p> <button @click = "count += 1">버튼을 눌러주세요</button>
-  <p>{{count}}</p>
+<template class="is-align-bottom">
+  <div>
+
+  <div>
+    <el-text  class="mx-1" type="primary">제목</el-text>
+ <el-input v-model = "title" placeholder="제목을 입력하세여"></el-input>
+  </div>
+
+  <div>
+    <el-input
+        v-model="content"
+        :rows="15"
+        type="textarea"
+        placeholder="Please input"
+    />
+  </div>
+    <el-button type="primary" @click = "write()">글 작성 완료</el-button>
+
+  </div>
 </template>
 
 
